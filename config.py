a69e97500b36eac44045ef44a80c2294adffde3a
ffggghamza9-dot <ffggghamza9@gmail.com>
2025-08-13 20:24:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_TOKEN = "PUT_YOUR_TOKEN_HERE"
+BOT_TOKEN = "8168891066:AAEO5G1AGN2YNg1Hz_z4iG_iVdfm5bfgiwo"
 # list of admin IDs (integers)
-ADMIN_IDS = [123456789]
+ADMIN_IDS = [7944027261]
 DB_PATH = "data/database.sqlite"
 MEDIA_FOLDER = "data/media/"
